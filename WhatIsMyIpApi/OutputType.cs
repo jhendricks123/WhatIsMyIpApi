2744665c6f5c73adab4206b1dad1088f875d4c84
Josh Hendricks <jhendricks123@gmail.com>
2017-05-16 21:58:25 -0700

    init

---
+++ WhatIsMyIpApi/OutputType.cs
@@ -0,0 +1,13 @@
+﻿namespace WhatIsMyIpApi
+{
+    public class OutputType
+    {
+        private OutputType(string value) => Value = value;
+        public string Value { get; private set; }
+
+        public static OutputType Text => new OutputType("");
+        public static OutputType Xml => new OutputType("xml");
+        public static OutputType Json => new OutputType("json");
+        public static OutputType Csv => new OutputType("csv");
+    }
+}
---
+++ WhatIsMyIpApi/WhatIsMyIp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace WhatIsMyIpApi
+{
+    public static class WhatIsMyIp
+    {
+        private const string GetIpApiString = "http://api.whatismyip.com/ip.php?key={0}";
+        private const string GetIpAddressLookupApiString = "http://api.whatismyip.com/ip-address-lookup.php?key={0}&input={1}&output={2}";
+
+        public static string GetIp(string apiKey)
+        {
+            return GetIpAsync(apiKey).Result;
+        }
+
+        public static async Task<string> GetIpAsync(string apiKey)
+        {
+            using (var client = new WebClient())
+                return await client.DownloadStringTaskAsync(new Uri(string.Format(GetIpApiString, apiKey)));
+        }
+
+        public static string GetIpAddressLookup(string apiKey, string ip, OutputType outputType)
+        {
+            return GetIpAddressLookupAsync(apiKey, ip, outputType).Result;
+        }
+        
+        public static async Task<string> GetIpAddressLookupAsync(string apiKey, string ip, OutputType outputType)
+        {
+            using (var client = new WebClient())
+                return await client.DownloadStringTaskAsync(new Uri(string.Format(GetIpAddressLookupApiString, apiKey, ip, outputType.Value)));
+        }
+    }
+}
